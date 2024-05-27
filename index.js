1d504973f3c709f136e94f2a3b13953027d67679
Abdurleone <abyyyusuf@gmail.com>
2024-05-27 17:01:46 +0300

    Project

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
 const app = express()
+dotenv.config()
 
-app.listen(8800, () =>{
+const connect = async ()=>{
+    try {
+        await mongoose.connect(process.env.MONGO);
+    } catch (error) {
+        throw Error
+    }
+};
+
+app.listen(8800, () => {
+    connect()
     console.log("Connected to backend!" )
 })
